199ea1a259d6b3f5bab7475155e3b1c5eee43356
James Hughes <james@pyrosoftsolutions.co.uk>
2023-01-21 12:25:14 +0000

    Added reference frames to enum

--- src/Astrodynamics/Enums/ReferenceFrame.cs
+++ src/Astrodynamics/Enums/ReferenceFrame.cs
@@ -2,5 +2,11 @@ namespace Astrodynamics.Enums;
 
 public enum ReferenceFrame
 {
-	
+	InternationalCelestialReferenceFrame,
+	J2000,
+	TrueEquatorMeanEquinox,
+	EarthCenteredInertial,
+	EarthCenteredEarthFixed,
+	Topocentric,
+	SpacecraftFixed
 }
